464c684e1e0a053f25cc57a7a6f9be56ae47fe7a
kanchika-kapoor <kanchika.ai@gmail.com>
2024-03-14 05:02:32 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import {Text} from '@mantine/core'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Text>Hello!</Text>
+    </>
+  )
+}
+
+export default App
